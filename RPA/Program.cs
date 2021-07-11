49aafbc6a233aa592e2dd5a7526dde790c48a92e
Jorge Pache <20190605@unapec.edu.do>
2021-07-11 00:44:28 -0400

    Agregado, using y sleeptime

--- RPA/Program.cs
+++ RPA/Program.cs
@@ -9,8 +9,8 @@ namespace RPA
     {
         static void Main(string[] args)
         {
-            String user = "";
-            String pass = "";
+            String user = "j.pache2@unapec.edu.do";
+            String pass = "P@ulin023!";
 
             //Desktop como carpeta de descarga
             var options = new ChromeOptions();
@@ -19,52 +19,55 @@ namespace RPA
 
             try
             {
-                ChromeDriver driver = new ChromeDriver(options);
-                driver.Manage().Window.Maximize();
+                using (ChromeDriver driver = new ChromeDriver(options))
+                {
+                    int sleepTime = 5000;
+                    driver.Manage().Window.Maximize();
 
-                ////Ingresar a CANVAS
-                driver.Url = "https://canvas.unapec.edu.do/";
+                    ////Ingresar a CANVAS
+                    driver.Url = "https://canvas.unapec.edu.do/";
 
-                //Seleccionar modulo de estudiantes
-                Thread.Sleep(5000);
-                driver.FindElement(By.CssSelector("body > header > div > div.row.header-info > " +
-                    "div.botones-opcion.col-12 > div > div:nth-child(1) > a")).Click();
+                    //Seleccionar modulo de estudiantes
+                    Thread.Sleep(sleepTime);
+                    driver.FindElement(By.CssSelector("body > header > div > div.row.header-info > " +
+                        "div.botones-opcion.col-12 > div > div:nth-child(1) > a")).Click();
 
-                //Ingresar correo
-                Thread.Sleep(5000);
-                driver.FindElement(By.CssSelector("#i0116")).SendKeys(user);
-                driver.FindElement(By.CssSelector("#idSIButton9")).Click();
+                    //Ingresar correo
+                    Thread.Sleep(sleepTime);
+                    driver.FindElement(By.CssSelector("#i0116")).SendKeys(user);
+                    driver.FindElement(By.CssSelector("#idSIButton9")).Click();
 
-                //Ingresar contraseña
-                Thread.Sleep(5000);
-                driver.FindElement(By.CssSelector("#i0118")).SendKeys(pass);
-                driver.FindElement(By.CssSelector("#idSIButton9")).Click();
+                    //Ingresar contraseña
+                    Thread.Sleep(sleepTime);
+                    driver.FindElement(By.CssSelector("#i0118")).SendKeys(pass);
+                    driver.FindElement(By.CssSelector("#idSIButton9")).Click();
 
-                //No recordar contraseña
-                Thread.Sleep(5000);
-                driver.FindElement(By.CssSelector("#idBtn_Back")).Submit();
+                    //No recordar contraseña
+                    Thread.Sleep(sleepTime);
+                    driver.FindElement(By.CssSelector("#idBtn_Back")).Submit();
 
-                //Seleccionar la primera materia
-                Thread.Sleep(5000);
-                //driver.FindElement(By.CssSelector("#DashboardCard_Container > div > div > div:nth-child(1)")).Click();
-                driver.FindElement(By.Id("global_nav_courses_link")).Click();
-                Thread.Sleep(1000);
-                driver.FindElement(By.CssSelector("#nav-tray-portal > span > span > div > " +
-                    "div > div > div > div > ul:nth-child(3) > li:nth-child(1) > a")).Click();
+                    //Seleccionar la primera materia
+                    Thread.Sleep(sleepTime);
+                    //driver.FindElement(By.CssSelector("#DashboardCard_Container > div > div > div:nth-child(1)")).Click();
+                    driver.FindElement(By.Id("global_nav_courses_link")).Click();
+                    Thread.Sleep(1000);
+                    driver.FindElement(By.CssSelector("#nav-tray-portal > span > span > div > " +
+                        "div > div > div > div > ul:nth-child(3) > li:nth-child(1) > a")).Click();
 
-                //Ir a archivos
-                Thread.Sleep(5000);
-                driver.FindElement(By.ClassName("files")).Click();
+                    //Ir a archivos
+                    Thread.Sleep(sleepTime);
+                    driver.FindElement(By.ClassName("files")).Click();
 
-                //Visualizar el primer archivo
-                Thread.Sleep(5000);
-                driver.FindElement(By.CssSelector("#content > div > div.ef-main > div > div > div > div:nth-child(3) > div.ef-name-col > a")).Click();
+                    //Visualizar el primer archivo
+                    Thread.Sleep(sleepTime);
+                    driver.FindElement(By.CssSelector("#content > div > div.ef-main > div > div > div > div:nth-child(3) > div.ef-name-col > a")).Click();
 
-                //Descargar
-                Thread.Sleep(5000);
-                driver.FindElement(By.CssSelector("body > span > span > span > div > div.ef-file-preview-header > div > a > span")).Click();
+                    //Descargar
+                    Thread.Sleep(sleepTime);
+                    driver.FindElement(By.CssSelector("body > span > span > span > div > div.ef-file-preview-header > div > a > span")).Click();
 
-                //driver.Close();
+                    //driver.Close();
+                }
             }
             catch(Exception ex)
             {
